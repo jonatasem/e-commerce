bf5ab493906fa80dd40b95cfbc67e1cb1d9cac81
jonatasem <jonatas.em20@gmail.com>
2024-09-30 21:03:37 -0300

    deploy

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { removeItem, changeQuantity } from '../redux/cartSlice';
-import { IoMdLogOut } from "react-icons/io";
 import './Cart.scss';
+
+import emptyCart from '../assets/img/empty-cart.png';
 
 const Cart = () => {
   const cartItems = useSelector(state => state.cart.items);
@@ -10,35 +11,37 @@ const Cart = () => {
 
   if (cartItems.length === 0) {
     return (
-      <section className='container-cart'>
-        <h2>Carrinho</h2>
-        <p>O carrinho está vazio.</p>
-        <a href="">Adicionar produtos</a>
-      </section>
+      <>
+        <section className='container-cart'>
+          <h2 className='title-cart'>Carrinho</h2>
+          <img className='empty' src={emptyCart} alt="carrinho vazio" />
+          <a className='aviso' href="">Comprar Agora</a>
+        </section>
+      </>
     );
   }
 
   return (
-    <section className='container-cart'>
-      <span className='exit-cart'>
-        <IoMdLogOut />
-      </span>
-      <h2>Carrinho</h2>
-      {cartItems.map(item => (
-        <article key={item.id}>
-          <h3>{item.name}</h3>
-          <p>Preço: ${item.price}</p>
-          <p>Quantidade: {item.quantity}</p>
-          <button onClick={() => dispatch(changeQuantity({ id: item.id, quantity: item.quantity + 1 }))}>+</button>
-          <button onClick={() => {
-            if (item.quantity > 1) {
-              dispatch(changeQuantity({ id: item.id, quantity: item.quantity - 1 }));
-            }
-          }}>-</button>
-          <button onClick={() => dispatch(removeItem({ id: item.id }))}>Remover</button>
-        </article>
-      ))}
-    </section>
+    <>
+      <section className='container-cart'>
+        <h2 className='title-cart'>Carrinho</h2>
+        {cartItems.map(item => (
+          <article key={item.id}>
+            <img src={item.img} alt="image do produto" />
+            <h3>{item.name}</h3>
+            <p className='value-cart'>Preço: ${item.price}</p>
+            <p className='amount-cart'>Quantidade: {item.quantity}</p>
+            <button onClick={() => dispatch(changeQuantity({ id: item.id, quantity: item.quantity + 1 }))}>+</button>
+            <button onClick={() => {
+              if (item.quantity > 1) {
+                dispatch(changeQuantity({ id: item.id, quantity: item.quantity - 1 }));
+              }
+            }}>-</button>
+            <button onClick={() => dispatch(removeItem({ id: item.id }))}>Remover</button>
+          </article>
+        ))}
+      </section>
+    </>
   );
 };
 
